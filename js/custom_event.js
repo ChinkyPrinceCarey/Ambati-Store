9cea15312fa3e87920a5949a7f978536d642f5d9
iMickey <skonlinedeals@gmail.com>
2022-07-29 10:35:38 +0530

    cleaned code

--- js/custom_event.js
+++ js/custom_event.js
@@ -3,44 +3,39 @@ function EventTarget(){
 }
 
 EventTarget.prototype = {
-
   constructor: EventTarget,
-
   addListener: function(type, listener){
       if (typeof this._listeners[type] == "undefined"){
           this._listeners[type] = [];
       }
-
       this._listeners[type].push(listener);
   },
-
   fire: function(event){
-      if (typeof event == "string"){
-          event = { type: event };
+      if(typeof event == "string"){
+        event = { type: event };
       }
-      if (!event.target){
-          event.target = this;
-      }
-
-      if (!event.type){  //falsy
-          throw new Error("Event object missing 'type' property.");
+      if(!event.target){
+        event.target = this;
       }
 
-      if (this._listeners[event.type] instanceof Array){
+      if(!event.type){
+        throw new Error("Event object missing 'type' property.");
+      }
+
+      if(this._listeners[event.type] instanceof Array){
           var listeners = this._listeners[event.type];
-          for (var i=0, len=listeners.length; i < len; i++){
-              listeners[i].call(this, event);
+          for(var i=0, len=listeners.length; i < len; i++){
+            listeners[i].call(this, event);
           }
       }
   },
-
   removeListener: function(type, listener){
-      if (this._listeners[type] instanceof Array){
+      if(this._listeners[type] instanceof Array){
           var listeners = this._listeners[type];
-          for (var i=0, len=listeners.length; i < len; i++){
-              if (listeners[i] === listener){
-                  listeners.splice(i, 1);
-                  break;
+          for(var i=0, len=listeners.length; i < len; i++){
+              if(listeners[i] === listener){
+                listeners.splice(i, 1);
+                break;
               }
           }
       }
--- js/parse_data.js
+++ js/parse_data.js
@@ -1,17 +1,5 @@
 function get_content(callback){
-	//let domain = "https://ambatitastyfoods.com";
-	
-	//let url = "./api/parse_data.php";
-	let url = "./sms/lib/items.php";
-	if(document.domain == "localhost"){
-		url = "http://localhost/web-dev/ambati-tasty-foods" + url;
-	}
-	
-	const formData = new FormData();
-	formData.append('action', 'fetch_for_app');
-	formData.append('data', 'random_data');
-	
-	//await fetch("http://ambatitastyfoods.com/sms/lib/items.php", );
+	let url = "./lib/items.php";
 	
 	fetch(url, {
     "credentials": "omit",
@@ -23,34 +11,29 @@ function get_content(callback){
         "Pragma": "no-cache",
         "Cache-Control": "no-cache"
     },
-    "referrer": "http://ambatitastyfoods.com/app.html",
     "body": "action=fetch_for_app&data=random_data",
     "method": "POST",
     "mode": "cors"
 	})
-	  .then(function(response){
+	.then(function(response){
 		return response.json();
-	  })
-	  .then(function(text){
-		  //console.log(text);
-		  callback(text);
-	  })
-	  .catch(function(error){
+	})
+	.then(function(text){
+		callback(text);
+	})
+	.catch(function(error){
 		console.log('Request failed', error)
-	  });
+	});
 }
 
 function render_content(_content){
-	// Template
-	//console.log(_content);
 	var template = document.getElementById('content-template').innerHTML;
 	var compiled = Template7(template).compile();
 	var compiledRendered = compiled(_content);
 	
 	$('#content-rendered').prepend(compiledRendered);
 	
-	//will replicates "onReady" 
-	//but using we can manually fire
+	//this replicates "onReady"
 	target.fire("onApiReady");
 }
 
