a89f22a8ead3e4b2f5fa7361396aa12ceeb8f5fa
iMickey <skonlinedeals@gmail.com>
2022-08-05 19:11:29 +0530

    updated `orders` code

--- lib/define_defaults.php
+++ lib/define_defaults.php
@@ -17,6 +17,7 @@ define("LIB_BASE_API", HOST_BASE_API . "/lib");
 define("REMOTE_SERVER_API_ENDPOINT", "https://ambatitastyfoods.com/v2/lib");
 define("REMOTE_SERVER_ITEMS_API_ENDPOINT", REMOTE_SERVER_API_ENDPOINT . "/items.php");
 define("REMOTE_SERVER_CUSTOMERS_API_ENDPOINT", REMOTE_SERVER_API_ENDPOINT . "/customers.php");
+define("REMOTE_SERVER_ORDERS_API_ENDPOINT", REMOTE_SERVER_API_ENDPOINT . "/orders.php");
 
 //fields config
 $fields_defination = json_decode(file_get_contents('./fields_defination.json'), true);
