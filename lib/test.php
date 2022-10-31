eac4efa71836a218ca36c5f602cc39c06781c845
iMickey <skonlinedeals@gmail.com>
2022-10-31 15:52:20 +0530

    Update test.php

--- lib/test.php
+++ lib/test.php
@@ -3,11 +3,11 @@ require_once('define_defaults.php');
 require_once('functions.php');
 ?>
 <?php
-$arr = array('name' => 'mickey');
-unset($arr['name']);
-print_r($arr);
 
-//mkdir('uploa');
-var_dump(json_decode("[1]", true));
+//$var_post = '{"data":[{"id":"","slno":"","material":"manufactured","item":"SOMEHTHI","shortcode":"OEMOT","type":"biscuit","unit":"cotton","sub_name":"","company_name":"","flavour":"","denomination":"","no_of":"1","counting":"0","level":"level_1","making_cost":"","retailer_cost":"","available_stock":"","actual_cost":"120","cost":"150","in_stock":"1","priority":"default","image":"1"}],"action":"create"}';
+
+//$insert_result = curl_request(REMOTE_SERVER_ITEMS_API_ENDPOINT, json_decode($var_post));
+
+//echo "<pre>"; print_r($insert_result); echo "</pre>";
 
 ?>
