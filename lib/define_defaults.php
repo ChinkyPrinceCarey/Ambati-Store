94156655868487e5be0b79f1dadc4110be933c19
iMickey <skonlinedeals@gmail.com>
2022-12-08 16:17:29 +0530

    Create define_defaults.php

--- lib/define_defaults.php
+++ lib/define_defaults.php
@@ -1,9 +1,27 @@
 <?php
+/*
 //database configuration
 define("DB_SERVER", "localhost");
 define("DB_USERNAME", "root");
 define("DB_PASSWORD", "");
 define("DB_DBNAME", "u916003822_ambati_beta");
+*/
+
+/*
+//server database configuration
+define("DB_SERVER", "217.21.88.3");
+define("DB_USERNAME", "u916003822_mickey_admin");
+define("DB_PASSWORD", "1@Passwordtrue");
+define("DB_DBNAME", "u916003822_ambati_sms");
+*/
+
+//
+//test-server database configuration
+define("DB_SERVER", "217.21.88.3");
+define("DB_USERNAME", "u916003822_mickey_beta");
+define("DB_PASSWORD", "123@Passwordtrue");
+define("DB_DBNAME", "u916003822_ambati_beta");
+//
 
 //## default directories ## //
 define("BASE_DIR", "C:/wamp64/www/ambati_sms/v2");
@@ -14,7 +32,7 @@ define("UPLOADS_DIR", "/" . UPLOADS_DIRNAME);
 
 define("HOST_BASE_API", "http://localhost/ambati_sms/v2");
 define("LIB_BASE_API", HOST_BASE_API . "/lib");
-define("REMOTE_SERVER_API_ENDPOINT", "https://ambatitastyfoods.com/v2/lib");
+define("REMOTE_SERVER_API_ENDPOINT", "https://ambatitastyfoods.com/lib");
 define("REMOTE_SERVER_ITEMS_API_ENDPOINT", REMOTE_SERVER_API_ENDPOINT . "/items.php");
 define("REMOTE_SERVER_CUSTOMERS_API_ENDPOINT", REMOTE_SERVER_API_ENDPOINT . "/customers.php");
 define("REMOTE_SERVER_ORDERS_API_ENDPOINT", REMOTE_SERVER_API_ENDPOINT . "/orders.php");
