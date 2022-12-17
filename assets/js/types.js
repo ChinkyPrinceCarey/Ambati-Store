7a9272c3c5137666b8704d46a45a1d6e0a28805b
iMickey <skonlinedeals@gmail.com>
2022-12-17 11:24:15 +0530

    point `type` operations on remote server

--- assets/js/types.js
+++ assets/js/types.js
@@ -25,7 +25,7 @@ $(function(){
         ajax: function(method, url, data, success, error){
             $.ajax({
                 type: "POST",
-                url:  "lib/types.php",
+                url:  API_ENDPOINT + "lib/types.php",
                 data: data,
                 dataType: "json",
                 success: function(json){
@@ -138,7 +138,7 @@ $(function(){
             ],
         },
         "ajax": {
-            "url": "lib/types.php",
+            "url": API_ENDPOINT + "lib/types.php",
             "type": "POST",
             "data" : {"action": "fetch_all", "data": "random_data"},
             "dataType": 'json',
