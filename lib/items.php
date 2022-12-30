f2ae276cf56bae148deacf9105590978ba7c6f40
iMickey <skonlinedeals@gmail.com>
2022-12-30 13:53:51 +0530

    out of stock items are in sync with warehouse stock
    created a SQL view `items_live_stock` using that fetching items in sync with warehouse stock

--- lib/items.php
+++ lib/items.php
@@ -230,18 +230,28 @@ if(isset($_POST['data']) && !empty($_POST['data'])){
             "
             SELECT * FROM (
                 SELECT * FROM (
-                    SELECT * FROM `items` WHERE `priority` != 'default' AND `material` != 'raw' AND `in_stock` != 0 ORDER BY `datetime` DESC
-                ) AS `t1` 
-            ORDER BY FIELD(`priority`, 'top', 'new', 'offer')
-            ) `t2`
+                    SELECT * FROM `items_live_stock` 
+                        WHERE `priority` != 'default'
+                            AND `available_stock` > 0  
+                            AND `in_stock` = 1
+                        ORDER BY `datetime` DESC
+                ) AS `t1_1`
+                ORDER BY FIELD(`priority`, 'top', 'new', 'offer')
+            ) AS `t1`
             
-            UNION
+            UNION 
             
-            SELECT * FROM (SELECT * FROM `items` WHERE `priority` = 'default' AND `material` != 'raw' AND `in_stock` = 1 ORDER BY rand()) `t3`
-
-            UNION
+            SELECT * FROM 
+            (
+                SELECT * FROM `items_live_stock` WHERE `priority` = 'default' AND `available_stock` > 0  AND `in_stock` = 1 ORDER BY rand()
+            ) AS `t3`
             
-            SELECT * FROM (SELECT * FROM `items` WHERE `priority` = 'default' AND `material` != 'raw' AND `in_stock` = 2) `t4`
+            UNION 
+            
+            SELECT * FROM 
+            (
+                SELECT * FROM `items_live_stock` WHERE `priority` = 'default' AND `available_stock` IS NULL  OR `in_stock` = 2 ORDER BY rand()
+            ) AS `t4`
             ";
 			
             $select_query = get_query($query_type, $query_table, $query_text);
