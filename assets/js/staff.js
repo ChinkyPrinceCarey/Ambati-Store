38d17f4d41d064798dc97fca86c5c6fd2a6aef64
iMickey <skonlinedeals@gmail.com>
2023-01-14 23:11:32 +0530

    +staff module
    from this scale to give individual logins to various users who are the staff

---
+++ assets/js/staff.js
@@ -0,0 +1,279 @@
+var table;
+var editor;
+var tableDom;
+
+$(function(){
+
+    API_ENDPOINT = ""; //that reset the api to local server
+
+    $.fn.search.settings.showNoResults = false;
+
+    tableDom = 
+    "<'ui stackable grid'"+
+        "<'row'"+
+            "<'eight wide column'B>"+
+        ">"+
+        "<'row'"+
+            "<'eight wide column'l>"+
+            "<'right aligned eight wide column'f>"+
+        ">"+
+        "<'row dt-table'"+
+            "<'sixteen wide column'tr>"+
+        ">"+
+        "<'row'"+
+            "<'seven wide column'i>"+
+            "<'right aligned nine wide column'p>"+
+        ">"+
+    ">";
+
+    editor = new $.fn.dataTable.Editor({
+        table: "#example",
+        fields: [
+            {
+                label: "Id",
+                name: "id"
+            },
+            {
+                label: "Slno",
+                name: "slno"
+            },
+            {
+                label: "Name",
+                name: "name",
+                attr: {
+                  required: true
+                }
+            },
+            {
+                label: "Email Id",
+                name: "email_id",
+                attr: {
+                  required: true
+                }
+            },
+            {
+                label: "Mobile Number",
+                name: "mobile_number",
+                attr: {
+                  required: true
+                }
+            },
+            {
+                label: "Roles",
+                name: "roles",
+                type: "select_multiple",
+                attr: {
+                  required: true
+                }
+            }
+        ],
+        ajax: function(method, url, data, success, error){
+            $.ajax({
+                type: "POST",
+                url:  `${LIB_API_ENDPOINT}/staff.php`,
+                data: data,
+                dataType: "json",
+                success: function(json){
+                    if(!json.result){
+                        console.log("error", json.info)
+                        error(json.info);
+                        return false;
+                    }
+                    success(json);
+                },
+                error: function(jqXHR, exception) {
+                    console.log(jqXHR, exception);
+                    error(exception);
+                },
+            });
+        },
+        i18n: {
+            error: {
+                system: "Parsing error, please reload the page and report to the admin!"
+            }
+        },
+        idSrc: 'id',
+        formOptions: {
+          main: {
+            onBackground: 'none',
+            //onBlur: 'none'
+          }
+        }
+    })
+
+    editor.on('processing', function(a, b){
+        if(b){
+            $('div.DTE_Form_Buttons button')
+            .addClass("loading")
+            .attr('disabled', true); //enable editor form submit button
+        }else{
+            $('div.DTE_Form_Buttons button')
+            .removeClass("loading")
+            .attr('disabled', false); //enable editor form submit button
+        }
+    });
+
+    editor.on('opened', function(a, b, action){
+        editor.field('id').hide();
+        editor.field('slno').hide();
+    });
+    
+    editor.on( 'preSubmit', function (e, data, action) {
+        if(action == "remove") return true;
+
+        return (function checkValidity(form, scope) {
+            var input
+            if (!form.checkValidity()) {
+              for (var i = 0; i < form.length; i++) {
+                // check for each field
+                input = form[i]
+                if (!input.checkValidity() && input.name) {
+                  scope.field(input.name).error(input.validationMessage)
+                }
+              }
+              return false
+            }
+            return true  
+        })(editor.dom.form, this);
+    });
+
+    editor.on('onSubmitError', function(e, xhr, err, thrown, data){
+        this.error("Parsing error, report to admin!");
+        return false;
+    });
+
+    editor.on('onSubmitComplete', function(e, xhr, err, thrown, data){
+        this.error(xhr);
+        return false;
+    });
+
+    table = $("#example").DataTable({
+        responsive: true,
+        processing: true,
+        serverSide: false, /* this will affect working of filtering */
+        autoWidth: true, /* true => disables width calc hence saves optimisation  */
+        select: true,
+        select: {
+            style: 'os',
+            className: 'primary'
+        },
+        dom: tableDom,
+        buttons: {
+            dom: {
+                button: {
+                  tag: 'button',
+                  className: ''
+                }
+            },
+            buttons: [
+                {
+                    extend: 'create', editor: editor, className: "positive ui button"
+                },
+                {
+                    extend: 'edit', editor: editor, className: "primary ui button"
+                },
+                {
+                    extend: 'remove', editor: editor, className: "negative ui button"
+                }
+            ],
+        },
+        "ajax": {
+            "url": `${LIB_API_ENDPOINT}/staff.php`,
+            "type": "POST",
+            "data" : {"action": "fetch_all", "data": "random_data"},
+            "dataType": 'json',
+            "dataSrc": function(json){
+                if(!json.result){
+                    smallModal(
+                        "Error Fetching Records", 
+                        json.info,
+                        [
+                            {
+                                "class": "ui positive approve button",
+                                "id": "",
+                                "text": "Okay",
+                            }
+                        ], 
+                        {
+                            closable: false,
+                            onApprove: function(){
+                                return true;
+                            }
+                        }
+                    );
+                }
+                return json.data;
+            },
+            "error": function(jqXHR, status, err){
+                console.log(jqXHR, status, err);
+                smallModal(
+                    status, 
+                    "Reload the page", 
+                    [
+                        {
+                            "class": "ui positive approve button",
+                            "id": "",
+                            "text": "Reload",
+                        }
+                    ], 
+                    {
+                        closable: false,
+                        onApprove: function(){
+                            window.location.replace(getCurrentPage());
+                            return false;
+                        }
+                    }
+                );
+            }
+        },
+        columns: [
+            { data: "slno", "width": "2%" },
+            { data: "name" },
+            { data: "email_id" },
+            { data: "mobile_number" },
+            { data: "roles" },
+            { data: "created_at" },
+            { data: "updated_at" }
+        ],
+        rowId: 'id',
+        "initComplete": function( settings, json ) {
+        }
+    });
+
+    table.on('init.dt', function(){});
+
+    //to disable `edit` button on when multiple rows selected
+    table.on('select', function(e, f, g){
+        var buttons = table.buttons( ['.buttons-edit'] );
+        var data = table.rows( { selected: true } ).data();
+        if(data.length > 1){
+            buttons.disable();
+        }else{
+            buttons.enable();
+        }
+    });
+
+    // Edit a record
+    $('#example').on('click', 'td .actions .ellipse-action-edit', function(e){
+        e.preventDefault();
+ 
+        editor.edit( $(this).closest('tr'),{
+            title: 'Edit Record',
+            buttons: 'Update'
+        });
+    });
+
+    // Delete a record
+    $('#example').on('click', 'td .actions .ellipse-action-remove', function(e){
+        e.preventDefault();
+ 
+        editor.remove( $(this).closest('tr'), {
+            title: 'Delete record',
+            message: 'Are you sure you wish to remove this record?',
+            buttons: 'Delete'
+        } );
+    });
+
+    //table.buttons().container().appendTo( $('div.eight.column:eq(0)', table.table().container()) );
+    table.buttons(0, null).container().removeClass('ui');
+});
